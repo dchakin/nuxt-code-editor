14a49cc678798bf25701350e9c8ef270b89f7c0c
dchakin <dmitrychakin27@gmail.com>
2025-12-01 16:10:20 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  app: {
+    head: {
+      title: 'Code Editor',
+    },
+  },
+
+  googleFonts: {
+    display: 'swap',
+    families: {
+      'Open Sans': {
+        wght: '300..800',
+        ital: '300..800'
+      }
+    }
+  },
+
+  modules: ['@nuxtjs/google-fonts'],
+})
