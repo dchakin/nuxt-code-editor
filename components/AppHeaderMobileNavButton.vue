e3c01f0dba2f0c0b999515ae48dd3537ac619e07
dchakin <dmitrychakin27@gmail.com>
2025-12-02 16:29:36 +0300

    add mobile nav components

---
+++ components/AppHeaderMobileNavButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <a class="-mr-1.5 rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+    <Icon :name="icon" class="size-6 dark:text-white" />
+  </a>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ icon: string }>()
+</script>
+
+<style></style>
---
+++ components/AppHeaderMobileNavItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="w-full">
+    <a href="#" :class="linkClasses">
+      <slot />
+    </a>
+  </li>
+</template>
+
+<script lang="ts" setup>
+const {
+  linkClasses = 'inline-block w-full rounded-md p-2 text-center text-sm font-semibold hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700',
+} = defineProps<{ linkClasses?: string }>()
+</script>
+
+<style></style>
