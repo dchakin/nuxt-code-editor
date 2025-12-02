76ec7f821e26dfdaef456c6cbc77b36ffb4a5450
dchakin <dmitrychakin27@gmail.com>
2025-12-02 16:02:30 +0300

    add section components

---
+++ components/AppHeroImage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="relative scale-90 lg:w-2/3 lg:scale-110 lg:perspective-distant">
+    <div class="hero-reflection"></div>
+    <img src="/editor.png" alt="Editor UI" class="hero-image" />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
