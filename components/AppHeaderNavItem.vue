f24b793a1780073958ac68bdb56ebbbe55c9de59
dchakin <dmitrychakin27@gmail.com>
2025-12-02 16:18:45 +0300

    add AppHeaderNavItem component

---
+++ components/AppHeaderNavItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <a href="#" :class="linkClasses">
+      <slot />
+    </a>
+  </li>
+</template>
+
+<script lang="ts" setup>
+const {
+  linkClasses = 'font-semibold hover:text-indigo-600 dark:text-gray-200 dark:hover:text-indigo-200',
+} = defineProps<{ linkClasses?: string }>()
+</script>
+
+<style></style>
