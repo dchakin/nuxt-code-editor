4c7ab87c657587dacfc621d8889052ed6be69873
dchakin <dmitrychakin27@gmail.com>
2025-12-03 17:18:49 +0300

    add playlist page

---
+++ components/PlaylistLesson.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col gap-y-6 md:gap-y-8 lg:col-span-2">
+    <PlaylistLessonVideo />
+    <PlaylistLessonContent />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
