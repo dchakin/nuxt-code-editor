5bfe52d03efbd91d72e2ec4703ca922db0465e68
dchakin <dmitrychakin27@gmail.com>
2025-12-03 16:48:54 +0300

    add playlist page components

---
+++ components/TagList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="flex flex-col gap-y-1">
+    <TagListItem v-for="tag in tags" :key="tag">{{ tag }}</TagListItem>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+const tags = [
+  'Productivity',
+  'Development',
+  'Design',
+  'Utility',
+  'Integration',
+  'Testing',
+  'Debugging',
+  'Formatting',
+  'Collaboration',
+  'Documentation',
+]
+</script>
+
+<style></style>
---
+++ components/TagListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+    <button
+      class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+    >
+      <slot />
+    </button>
+  </li>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
