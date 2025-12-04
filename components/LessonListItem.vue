8088deea76123b0d1a686f1dca17f5a201030a20
dchakin <dmitrychakin27@gmail.com>
2025-12-04 16:17:29 +0300

    add active lesson link

--- components/LessonListItem.vue
+++ components/LessonListItem.vue
@@ -1,9 +1,6 @@
 <template>
   <li>
-    <NuxtLink
-      :to="`/playlists/${playlistSlug}/lessons/${lessonSlug}`"
-      class="flex w-full items-center justify-between gap-x-4 rounded-lg bg-indigo-50 p-3 text-indigo-700 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-700"
-    >
+    <NuxtLink :to="`/playlists/${playlistSlug}/lessons/${lessonSlug}`" :class="linkClasses">
       <span class="truncate text-sm font-medium sm:text-base">
         {{ lesson.number }}. {{ lesson.title }}
       </span>
@@ -25,6 +22,13 @@ const props = defineProps<{
 
 const playlistSlug = useRoute().params.playlistSlug
 const lessonSlug = props.lesson.title.toLocaleLowerCase().replaceAll(' ', '-')
+
+const linkClasses = computed(() => [
+  'flex w-full items-center justify-between gap-x-4 rounded-lg p-3 dark:text-gray-300 dark:hover:bg-gray-700',
+  useRoute().params.lessonSlug === lessonSlug
+    ? 'bg-indigo-50 text-indigo-700 dark:bg-gray-700'
+    : 'text-gray-600 hover:bg-gray-100',
+])
 </script>
 
 <style></style>
