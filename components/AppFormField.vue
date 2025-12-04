bdd0a3fa73f3bae625c8d6d4f16c6cd41757cd91
dchakin <dmitrychakin27@gmail.com>
2025-12-04 17:23:09 +0300

    add form components

---
+++ components/AppFormField.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col gap-y-1">
+    <AppLabel :for="id">{{ label }}</AppLabel>
+    <AppInput :type="type" :id="id" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  label: string
+  type: string
+  id: string
+}>()
+</script>
+
+<style></style>
