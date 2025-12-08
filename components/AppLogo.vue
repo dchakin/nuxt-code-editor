43db67f72cf46afcdd51d5ab79a468ef9d6435a2
dchakin <dmitrychakin27@gmail.com>
2025-12-08 17:33:15 +0300

    section header component refactoring

---
+++ components/AppLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLink to="/" class="flex items-center gap-x-1 font-black sm:gap-x-2">
+    <Icon name="code" :class="`-rotate-6 ${iconClasses}`" />
+
+    <span v-if="$slots.default">
+      <slot />
+    </span>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ iconClasses: string }>()
+</script>
+
+<style></style>
