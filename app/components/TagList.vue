0fa0b1af8ba8992d448a3855545336c6bc5de5f2
dchakin <dmitrychakin27@gmail.com>
2025-12-11 17:45:14 +0300

    add tag selection

--- app/components/TagList.vue
+++ app/components/TagList.vue
@@ -1,11 +1,18 @@
 <template>
   <ul class="flex flex-col gap-y-1">
-    <TagListItem v-for="tag in tags" :key="tag.id">{{ tag.name }}</TagListItem>
+    <TagListItem
+      v-for="tag in tagsStore.tags"
+      :key="tag.id"
+      :selected="tagsStore.selectedTagIds.includes(tag.id)"
+      @click="tagsStore.toggleTag(tag.id)"
+    >
+      {{ tag.name }}
+    </TagListItem>
   </ul>
 </template>
 
 <script lang="ts" setup>
-const { tags } = useTagsStore()
+const tagsStore = useTagsStore()
 </script>
 
 <style></style>
